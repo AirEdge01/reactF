9583ce4488cc846ea5863b66c04f468eff36f9a7
Israel <israeloye2019@gmail.com>
2025-11-06 11:12:34 +0100

    reactfs

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
   return (
     <>
       <Routes>
-        <Route path='/Signup' element={<Signup/>} />
+        <Route path='/' element={<Signup/>} />
       </Routes>
     </>
   )
